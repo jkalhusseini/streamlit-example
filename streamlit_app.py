48090ea4f3bac4f6f0901b652a3e3cd10e27ca9b
jkalhusseini <jkae2018@mymail.pomona.edu>
2022-04-25 14:19:32 -0700

    Figure link

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,6 @@ with st.echo(code_location='below'):
     n_i = 1
 
 
-    Point = namedtuple('Point', 'x y')
-    data = []
-
     #Create drop down menu to select variables 
     options = st.multiselect(
      'Which variables would you like to select?',
@@ -50,14 +47,15 @@ with st.echo(code_location='below'):
         dRp = (Rp/3)[[(c1(g5+g6)-s*g1)/15][Rp*Rp]+(s*Phi_i)-(s*Phi_n)-[(s*2*ae)/Rp]-c1(n_i)-c2]
         return dRp
 
-    date = pd.DataFrame(TumorModel())
-
+    data = pd.DataFrame(TumorModel())
 
     import plotly.graph_objects as go
     import plotly.figure_factory as ff
     
-    fig = go.Figure(data=[go.surface(x = dRp, y = [VarSelector1], z = [VarSelector2])])
+    fig = go.Figure(data=[go.surface()])
     st.plotly_chart
+    fig.show()
+
 
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
