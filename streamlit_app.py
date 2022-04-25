1a4c4116db04df4fd3f3a4bf667e9a63eea5a8fa
jkalhusseini <jkae2018@mymail.pomona.edu>
2022-04-25 09:53:04 -0700

    Model input

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@ import streamlit as st
 
 
 with st.echo(code_location='below'):
+
+    #Clinical slider parameters
     g0 = st.slider("Rate of nutrient consumption", 1, 500, 250)
     g1 = st.slider("Nutrient supply degradation (ext. inhibitor)", 1, 10, 5)
     g2 = st.slider("External inhibitor degradation", 1, 10, 5)
@@ -14,10 +16,23 @@ with st.echo(code_location='below'):
     g5 = st.slider("Rate of NP binding", 1, 10, 5)
     g6 = st.slider("Rate of NP degradation", 1, 10, 5)
 
+    c1 = st.slider("Auger effect", 1, 10, 5)
+    c2 = st.slider("Radiotherapy effect", 1, 10, 5)
+
+    #Mathematical proportionality components
+    s = 100
+    ae = 0.2
+    Phi_i = 1
+    Phi_n = 0.6
+    n_i = 1
+
+
     Point = namedtuple('Point', 'x y')
     data = []
 
-    points_per_turn = total_points / num_turns
+    def TumorModel(Rp, c1, g5, g6, s, g1, Phi_i, Phi_n, ae, n_i, c2):
+        dRp = (Rp/3)[[(c1(g5+g6)-s*g1)/15][Rp*Rp]+(s*Phi_i)-(s*Phi_n)-[(s*2*ae)/Rp]-c1(n_i)-c2]
+
 
     for curr_point_num in range(total_points):
         curr_turn, i = divmod(curr_point_num, points_per_turn)
