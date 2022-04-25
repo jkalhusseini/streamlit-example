b52f800b98f43ac4a63f08d258a55ef77897b9ad
jkalhusseini <jkae2018@mymail.pomona.edu>
2022-04-25 10:03:35 -0700

    Attempt model implementation

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,18 +30,19 @@ with st.echo(code_location='below'):
     Point = namedtuple('Point', 'x y')
     data = []
 
-    def TumorModel(Rp, c1, g5, g6, s, g1, Phi_i, Phi_n, ae, n_i, c2):
+    def TumorModel(dRp, Rp, c1, g5, g6, s, g1, Phi_i, Phi_n, ae, n_i, c2):
         dRp = (Rp/3)[[(c1(g5+g6)-s*g1)/15][Rp*Rp]+(s*Phi_i)-(s*Phi_n)-[(s*2*ae)/Rp]-c1(n_i)-c2]
 
-
-    for curr_point_num in range(total_points):
+    for dRp in range(g0):
+        TumorModel()
+    """for curr_point_num in range(g0):
         curr_turn, i = divmod(curr_point_num, points_per_turn)
         angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
         radius = curr_point_num / total_points
         x = radius * math.cos(angle)
         y = radius * math.sin(angle)
         data.append(Point(x, y))
-
+"""
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
