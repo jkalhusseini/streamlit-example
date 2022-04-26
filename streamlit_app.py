6081f7625f6ffae3da654dd15a30cbfe150342c8
jkalhusseini <jkae2018@mymail.pomona.edu>
2022-04-26 08:59:35 -0700

    Plotting components

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,42 @@
-from collections import namedtuple
 import altair as alt
 import math
 from nbformat import write
 import pandas as pd
 import streamlit as st
 import numpy as np
+from scipy.integrate import odeint
+from matplotlib import pyplot as plt
 
 
 with st.sidebar:
 
     #Clinical slider parameters
-        g0 = st.slider("Rate of nutrient consumption", 1, 500, (20,100))
-        g1 = st.slider("Nutrient supply degradation (ext. inhibitor)", 1, 10, (1,5))
-        g2 = st.slider("External inhibitor degradation", 1, 10, (1,5))
-        g3 = st.slider("Inhibitor generation by tumor", 1, 10, (1,5))
-        g4 = st.slider("Inhibitor degradation by tumor", 1, 10, (1,5))
-        g5 = st.slider("Rate of NP binding", 1, 10, (1,5))
-        g6 = st.slider("Rate of NP degradation", 1, 10, (1,5))
+    g0 = st.slider("Rate of nutrient consumption", 1, 500, (20,100))
+    g1 = st.slider("Nutrient supply degradation (ext. inhibitor)", 1, 10, (1,5))
+    g2 = st.slider("External inhibitor degradation", 1, 10, (1,5))
+    g3 = st.slider("Inhibitor generation by tumor", 1, 10, (1,5))
+    g4 = st.slider("Inhibitor degradation by tumor", 1, 10, (1,5))
+    g5 = st.slider("Rate of NP binding", 1, 10, (1,5))
+    g6 = st.slider("Rate of NP degradation", 1, 10, (1,5))
 
-        c1 = st.slider("Auger effect", 1, 10, (1,5))
-        c2 = st.slider("Radiotherapy effect", 1, 10, (1,5))
+    c1 = st.slider("Auger effect", 1, 10, (1,5))
+    c2 = st.slider("Radiotherapy effect", 1, 10, (1,5))
 
     #Mathematical proportionality components
-        s = 100
-        ae = 0.2
-        Phi_i = 1 
-        Phi_n = 0.6
-        n_i = 1
+    s = 100
+    ae = 0.2
+    Phi_i = 1 
+    Phi_n = 0.6
+    n_i = 1
+    Rp = 1
         
 
-with st.echo(code_location='below'):
 
     #Create drop down menu to select variables 
-    options = st.multiselect(
+options = st.multiselect(
         'Which variables would you like to select?',
         ['g5', 'g6', 'c1', 'c2'])
-    if options :
+if options :
         if len(options) <= 2:
             st.write()
             #list_options = []
@@ -45,14 +46,30 @@ with st.echo(code_location='below'):
             st.warning("Please only select two variables")
 
     #Establishing user's first and second variables, to be used in 3D scatter plot
-    var1 = options[0]
-    var2 = options[1]
+var1 = options[0]
+var2 = options[1]
 
-    def TumorModel(Rp, c1, g5, g6, s, g1, Phi_i, Phi_n, ae, n_i, c2):
-        dRp = (Rp/3)[[(c1(g5+g6)-s*g1)/15][Rp*Rp]+(s*Phi_i)-(s*Phi_n)-[(s*2*ae)/Rp]-c1(n_i)-c2]
-        return dRp
+def model(Rp, var1, var2):
+    s = 100
+    ae = 0.2
+    Phi_i = 1 
+    Phi_n = 0.6
+    n_i = 1
+    Rp = 1
+    dRp = (Rp/3)[[(c1(g5+g6)-s*g1)/15][Rp*Rp]+(s*Phi_i)-(s*Phi_n)-[(s*2*ae)/Rp]-c1(n_i)-c2]
+    return dRp
+
+Rp0 = [0,50,100]
+t = np.linspace(0,20,200)
+result = odeint(model, Rp0, t, args=(var2,))
+
+fig,ax = plt.subplots()
+ax.plot(t,result[:,0],label='R0=0')
+ax.plot(t,result[:,1],label='R0=0.3')
+ax.plot(t,result[:,2],label='R0=1')
     
-    TumorModel(10, c1, g5, g6, s, g1, Phi_i, Phi_n, ae, n_i, c2)
-
-    fig = plot(x = dRp, y = var1, z = var2)
-    st.fig(write)
+"""def TumorModel(Rp, c1, g5, g6, s, g1, Phi_i, Phi_n, ae, n_i, c2):
+        dRp = (Rp/3)[[(c1(g5+g6)-s*g1)/15][Rp*Rp]+(s*Phi_i)-(s*Phi_n)-[(s*2*ae)/Rp]-c1(n_i)-c2]
+        return dRp"""
+    
+"""TumorModel(10, c1, g5, g6, s, g1, Phi_i, Phi_n, ae, n_i, c2)"""
