3cf7dabe20499e7bffcc57c0027e2014945178f5
jkalhusseini <jkae2018@mymail.pomona.edu>
2022-04-30 13:12:21 -0700

    renovations

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,88 +8,6 @@ from scipy.integrate import odeint
 from matplotlib import image, pyplot as plt
 import plotly.express as px
 import random
-
-
-
-with st.sidebar:
-
-    """g0 = st.slider("Rate of nutrient consumption", 1, 500)
-    g1 = st.slider("Nutrient supply degradation (ext. inhibitor)", 1, 10)
-    g2 = st.slider("External inhibitor degradation", 1, 10)
-    g3 = st.slider("Inhibitor generation by tumor", 1, 10)
-    g4 = st.slider("Inhibitor degradation by tumor", 1, 10)
-    g5 = st.slider("Rate of NP binding", 1, 10)
-    g6 = st.slider("Rate of NP degradation", 1, 10)
-    
-
-    c1 = st.slider("Auger effect", 1, 10)
-    c2 = st.slider("Radiotherapy effect", 1, 10)
-    """
-
-    g0 = random.randint(1,500)
-    g1 = random.randint(1,500)
-    g2 = random.randint(1,500)
-    g3 = random.randint(1,500)
-    g4 = random.randint(1,500)
-    g5 = random.randint(1,500)
-    g6 = random.randint(1,500)
-    
-
-    c1 = random.randint(1,500)
-    c2 = random.randint(1,500)
-
-
-    #Mathematical proportionality components
-    s = 100
-    ae = 0.2
-    Phi_i = 1 
-    Phi_n = 0.6
-    n_i = 1
-    Rp = 1
-
-def model(Rp, var1, var2):
-    s = 100
-    ae = 0.2
-    Phi_i = 1 
-    Phi_n = 0.6
-    n_i = 1
-    Rp = 1
-    dRp = (Rp/3)*(((
-        c1
-    *(g5+g6)-s*g1)*(0.0666666))*(Rp*Rp)+(s*Phi_i)-(s*Phi_n)-((s*2*ae)/Rp)-c1*(n_i)-c2)
-    #return dRp 
-
-
-    #Create drop down menu to select variables 
-options = st.multiselect(
-        'Which variables would you like to select?',
-        ['g5', 'g6', 'c1', 'c2'])
-st.write(options[0])
-
-if options :
-        if len(options) > 2:
-            st.warning("Too many variables")
-        elif len(options) < 2:
-            st.warning("Too few variables")
-            #list_options = []
-            #list_options.append(options)
-        #st.write('You selected:', options)
-        else:
-            new_list = []
-            var1 = options[0]
-            new_list.append(var1)
-            var2 = options[1]
-            new_list.append(var2)
-            model(Rp, var1, var2)
-            dRp = 0
-            new_list.append(dRp)
-
-#options = px.data.election()
-fig = px.scatter_3d(new_list, dRp, var1,var2, color="winner", size="total", hover_name="tumor growth rate",
-                  symbol="result", color_discrete_map = {Rp: "blue", var1: "green", var2:"red"})
-st.write(fig)
-        
-
 import time
 import numpy as np 
 from skimage import io
@@ -181,3 +99,82 @@ fig.update_layout(
 )
 
 fig.show()
+
+with st.sidebar:
+
+    """g0 = st.slider("Rate of nutrient consumption", 1, 500)
+    g1 = st.slider("Nutrient supply degradation (ext. inhibitor)", 1, 10)
+    g2 = st.slider("External inhibitor degradation", 1, 10)
+    g3 = st.slider("Inhibitor generation by tumor", 1, 10)
+    g4 = st.slider("Inhibitor degradation by tumor", 1, 10)
+    g5 = st.slider("Rate of NP binding", 1, 10)
+    g6 = st.slider("Rate of NP degradation", 1, 10)
+    
+
+    c1 = st.slider("Auger effect", 1, 10)
+    c2 = st.slider("Radiotherapy effect", 1, 10)
+    """
+
+    g0 = random.randint(1,500)
+    g1 = random.randint(1,500)
+    g2 = random.randint(1,500)
+    g3 = random.randint(1,500)
+    g4 = random.randint(1,500)
+    g5 = random.randint(1,500)
+    g6 = random.randint(1,500)
+    
+
+    c1 = random.randint(1,500)
+    c2 = random.randint(1,500)
+
+
+    #Mathematical proportionality components
+    s = 100
+    ae = 0.2
+    Phi_i = 1 
+    Phi_n = 0.6
+    n_i = 1
+    Rp = 1
+
+def model(Rp, var1, var2):
+    s = 100
+    ae = 0.2
+    Phi_i = 1 
+    Phi_n = 0.6
+    n_i = 1
+    Rp = 1
+    dRp = (Rp/3)*(((
+        c1
+    *(g5+g6)-s*g1)*(0.0666666))*(Rp*Rp)+(s*Phi_i)-(s*Phi_n)-((s*2*ae)/Rp)-c1*(n_i)-c2)
+    #return dRp 
+
+
+    #Create drop down menu to select variables 
+options = st.multiselect(
+        'Which variables would you like to select?',
+        ['g5', 'g6', 'c1', 'c2'])
+st.write(options[0])
+
+if options :
+        if len(options) > 2:
+            st.warning("Too many variables")
+        elif len(options) < 2:
+            st.warning("Too few variables")
+            #list_options = []
+            #list_options.append(options)
+        #st.write('You selected:', options)
+        else:
+            new_list = []
+            var1 = options[0]
+            new_list.append(var1)
+            var2 = options[1]
+            new_list.append(var2)
+            model(Rp, var1, var2)
+            dRp = 0
+            new_list.append(dRp)
+
+#options = px.data.election()
+fig = px.scatter_3d(new_list, dRp, var1,var2, color="winner", size="total", hover_name="tumor growth rate",
+                  symbol="result", color_discrete_map = {Rp: "blue", var1: "green", var2:"red"})
+st.write(fig)
+        
