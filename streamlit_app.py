90e4a550f60dfbf60cfeced376977075dcc34bb1
jkalhusseini <jkae2018@mymail.pomona.edu>
2022-04-30 19:42:47 -0700

    container attempt

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,9 @@ import plotly.figure_factory as ff
 import pandas as pd
 
 
-col1, col2 = st.columns(2)
 
 data = pd.DataFrame
-with st.sidebar:
+with st.container:
     g0 = random.randint(1,500)
     st.write(g0)
     g1 = random.randint(1,500)
@@ -62,7 +61,7 @@ def model(Rp, g1, c1, c2, g5, g6):
     st.write(dRp)
     data.append()
 
-with col2:
+with st.container:
     if st.button("Generate random parameters"):
         model(Rp, g1, c1, c2, g5, g6)
         
@@ -80,92 +79,93 @@ fig = ff.create_scatterplotmatrix(data, diag='histogram', index='Species',
                                   height=800, width=800)
 
 st.write(fig)
-        
 
-vol = io.imread("https://s3.amazonaws.com/assets.datacamp.com/blog_assets/attention-mri.tif")
-volume = vol.T
-r, c = volume[0].shape
+with st.container:
 
-# Define frames
-import plotly.graph_objects as go
-nb_frames = 68
+    vol = io.imread("https://s3.amazonaws.com/assets.datacamp.com/blog_assets/attention-mri.tif")
+    volume = vol.T
+    r, c = volume[0].shape
 
-fig = go.Figure(frames=[go.Frame(data=go.Surface(
-    z=(6.7 - k * 0.1) * np.ones((r, c)),
-    surfacecolor=np.flipud(volume[67 - k]),
-    cmin=0, cmax=200
-    ),
-    name=str(k) # you need to name the frame for the animation to behave properly
+    # Define frames
+    import plotly.graph_objects as go
+    nb_frames = 68
+
+    fig = go.Figure(frames=[go.Frame(data=go.Surface(
+        z=(6.7 - k * 0.1) * np.ones((r, c)),
+        surfacecolor=np.flipud(volume[67 - k]),
+        cmin=0, cmax=200
+        ),
+        name=str(k) # you need to name the frame for the animation to behave properly
+        )
+        for k in range(nb_frames)])
+
+    # Add data to be displayed before animation starts
+    fig.add_trace(go.Surface(
+        z=6.7 * np.ones((r, c)),
+        surfacecolor=np.flipud(volume[67]),
+        colorscale='blues',
+        cmin=0, cmax=200,
+        colorbar=dict(thickness=20, ticklen=4)
+        ))
+
+
+    def frame_args(duration):
+        return {
+                "frame": {"duration": duration},
+                "mode": "immediate",
+                "fromcurrent": True,
+                "transition": {"duration": duration, "easing": "linear"},
+            }
+
+    sliders = [
+                {
+                    "pad": {"b": 10, "t": 60},
+                    "len": 0.9,
+                    "x": 0.1,
+                    "y": 0,
+                    "steps": [
+                        {
+                            "args": [[f.name], frame_args(0)],
+                            "label": str(k),
+                            "method": "animate",
+                        }
+                        for k, f in enumerate(fig.frames)
+                    ],
+                }
+            ]
+
+    # Layout
+    fig.update_layout(
+            title='Volumetric MRI',
+            width=200,
+            height=200,
+            scene=dict(
+                        zaxis=dict(range=[-0.1, 6.8], autorange=False),
+                        aspectratio=dict(x=1, y=1, z=1),
+                        ),
+            updatemenus = [
+                {
+                    "buttons": [
+                        {
+                            "args": [None, frame_args(50)],
+                            "label": "&#9654;", # play symbol
+                            "method": "animate",
+                        },
+                        {
+                            "args": [[None], frame_args(0)],
+                            "label": "&#9724;", # pause symbol
+                            "method": "animate",
+                        },
+                    ],
+                    "direction": "left",
+                    "pad": {"r": 10, "t": 70},
+                    "type": "buttons",
+                    "x": 0.1,
+                    "y": 0,
+                }
+            ],
+            sliders=sliders
     )
-    for k in range(nb_frames)])
 
-# Add data to be displayed before animation starts
-fig.add_trace(go.Surface(
-    z=6.7 * np.ones((r, c)),
-    surfacecolor=np.flipud(volume[67]),
-    colorscale='blues',
-    cmin=0, cmax=200,
-    colorbar=dict(thickness=20, ticklen=4)
-    ))
-
-
-def frame_args(duration):
-    return {
-            "frame": {"duration": duration},
-            "mode": "immediate",
-            "fromcurrent": True,
-            "transition": {"duration": duration, "easing": "linear"},
-        }
-
-sliders = [
-            {
-                "pad": {"b": 10, "t": 60},
-                "len": 0.9,
-                "x": 0.1,
-                "y": 0,
-                "steps": [
-                    {
-                        "args": [[f.name], frame_args(0)],
-                        "label": str(k),
-                        "method": "animate",
-                    }
-                    for k, f in enumerate(fig.frames)
-                ],
-            }
-        ]
-
-# Layout
-fig.update_layout(
-        title='Volumetric MRI',
-        width=200,
-        height=200,
-        scene=dict(
-                    zaxis=dict(range=[-0.1, 6.8], autorange=False),
-                    aspectratio=dict(x=1, y=1, z=1),
-                    ),
-        updatemenus = [
-            {
-                "buttons": [
-                    {
-                        "args": [None, frame_args(50)],
-                        "label": "&#9654;", # play symbol
-                        "method": "animate",
-                    },
-                    {
-                        "args": [[None], frame_args(0)],
-                        "label": "&#9724;", # pause symbol
-                        "method": "animate",
-                    },
-                ],
-                "direction": "left",
-                "pad": {"r": 10, "t": 70},
-                "type": "buttons",
-                "x": 0.1,
-                "y": 0,
-            }
-        ],
-        sliders=sliders
-)
-
-fig.show()
+    fig.show()
 
